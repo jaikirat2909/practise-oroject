335fd147097bd9b5286b642841932e95dd62901d
jaikirat2909 <124550982+jaikirat2909@users.noreply.github.com>
2024-11-12 11:54:02 +0530

    12 11 24

--- servere/routes/userRoutes.js
+++ servere/routes/userRoutes.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const router = express.Router();
-//const  jwtAuthMiddleware= require("../middlewares/jwtmiddleware") ;
-const{validateToken,generateToken}=require("../middlewares/jwtmiddleware")
+const  User= require("../models/userModel") ;
+
+const myAccount=async(req,res)=>{
+    const {email} = req.body;
+    const user = await User.findOne({email});
+    if(user){
+        res.send(user);
+    }
+}
+// const{validateToken,generateToken}=require("../middlewares/jwtmiddleware")
 const {
     registerUser,
     loginUser
 }=require("../controllers/userController");
+const {validateJwtToken }=require("../middlewares/jwtmiddleware");
 //route for user registration 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
+router.get("/details",myAccount,validateJwtToken );
+router.put("/detailsUpdate");
 //route for user login
 //router.post("/register",loginUser);
 
